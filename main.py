abe80b972902d37abcb81ad3bf2f3315b9ccad1e
Mironenko <vladislava_mironenko@gmail.com>
2024-04-08 11:32:51 +0200

    submission

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ OrderOfNumbers
 # Provide your solution here
 
 
+n1 = input("Enter your first number:")
+n2 = input("Enter your second number:")
+sum = int(n1) + int(n2)
+
+print(sum)
+if int(n1) <= 0 or int(n2) <= 0:
+    print("smaller or equal to 0")
+elif int(n1) <= 0 and int(n2) <= 0:
+    print("smaller or equal to 0")
+elif int(n1) >= 0 and int(n2) >= 0 and int(n1) >= int(n2):
+    print("error")
 
 """
 SumUp
 """
 # Provide your solution here
+for i in range(3):    # For each number i in range 0-4. range(5) function returns list [0, 1, 2, 3, 4]
+    num = input("Enter your number:")
+    numbers = []
+    numbers.append(num)
